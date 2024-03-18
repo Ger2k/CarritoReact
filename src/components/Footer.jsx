c0917a69dda9e767f5d550eba0e1787973363bcf
Ger2k <musikadicto@gmail.com>
2024-03-18 16:29:36 +0100

    Carrito React funcional

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css'
+import { useFilters } from '../hooks/useFilters'
+import { useCart } from '../hooks/useCart'
+
+export function Footer () {
+  // const { filters } = useFilters()
+  const { cart } = useCart()
+
+  return (
+    <footer className='footer'>
+      <h4>Practica prueba técnica de React</h4>
+      <h5>Shopping Cart con useContext</h5>
+    </footer>
+  )
+}
