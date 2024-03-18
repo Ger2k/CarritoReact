3cc396c0eaa22e45a4417190959a199c789d687f
Ger2k <musikadicto@gmail.com>
2024-03-18 16:49:09 +0100

    Resuelto error nombre de archivos 4

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,47 +3,46 @@ import { AddToCartIcon, RemoveFromCartIcon } from './Icons.jsx'
 import { useCart } from '../hooks/useCart.js'
 
 export function Products ({ products }) {
+  const { addToCart, removeFromCart, cart } = useCart()
 
-    const { addToCart, removeFromCart, cart } = useCart()
+  const checkProductInCart = product => {
+    return cart.some(item => item.id === product.id)
+  }
 
-    const checkProductInCart = product => {
-        return cart.some(item => item.id === product.id)
-    }
-    
-    return (
-        <main className='products'>
-            <ul>
-                {products.slice(0,10).map(product => {
-                    const isProductInCart = checkProductInCart(product)
+  return (
+    <main className='products'>
+      <ul>
+        {products.slice(0, 10).map(product => {
+          const isProductInCart = checkProductInCart(product)
 
-                    return (
-                    <li key={product.id}>
-                        <img 
-                            src={product.thumbnail} 
-                            alt={product.title} 
-                        />
-                        <div>
-                            <strong>{product.title}</strong> - ${product.price}
-                        </div>
-                        <div>
-                            <button style={{ backgroundColor : isProductInCart ? "red" : "#09f" }} onClick={() => {
-                                isProductInCart
-                                ? removeFromCart(product)
-                                : addToCart(product)
-                            }}
-                            >
-                                
-                                {
-                                    isProductInCart
-                                    ? <RemoveFromCartIcon />
-                                    : <AddToCartIcon />
-                                }
-                            </button>
-                        </div>
-                    </li>
-                    )
-                })}
-            </ul>
-        </main>
-    )
+          return (
+            <li key={product.id}>
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+              />
+              <div>
+                <strong>{product.title}</strong> - ${product.price}
+              </div>
+              <div>
+                <button
+                  style={{ backgroundColor: isProductInCart ? 'red' : '#09f' }} onClick={() => {
+                    isProductInCart
+                      ? removeFromCart(product)
+                      : addToCart(product)
+                  }}
+                >
+                  {
+                    isProductInCart
+                      ? <RemoveFromCartIcon />
+                      : <AddToCartIcon />
+                  }
+                </button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </main>
+  )
 }
