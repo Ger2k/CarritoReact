9c1abcdfe6139f0559e657af4d1d9a7623ee9dbd
Ger2k <musikadicto@gmail.com>
2024-03-19 15:33:39 +0100

    Fondo de pantalla css grandient

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { IS_DEVELOPMENT } from './config.js'
 import { useFilters } from './hooks/useFilters.js'
 import { Cart } from './components/Cart.jsx'
 import { CartProvider } from './context/cart.jsx'
-
+import { Background } from './components/Background.jsx'
 
 function App() {
   const { filterProducts } = useFilters()
@@ -15,6 +15,7 @@ function App() {
 
   return (
     <CartProvider>
+      <Background />
       <Header />
       <Cart />
       <Products products={filteredProducts} />
---
+++ src/components/Background.jsx
@@ -0,0 +1,12 @@
+import './Background.css'
+
+export function Background(){
+    return (
+        <>
+            <div class="background-wrapper">
+                <div class="grid-overlay"></div>
+                <div class="radial-overlay"></div>
+            </div>
+        </>
+    )
+}
